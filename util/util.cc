a9ece76101ff2352f1b093ab63dc0c387c9074b4
Carlos Valera <cookyt2@gmail.com>
2013-01-18 13:12:53 -0500

    Sent file open error messages from sfope() to stderr

--- util/util.cc
+++ util/util.cc
@@ -6,7 +6,7 @@ FILE *sfopen(const char *path, const char *mode)
     FILE *fil = fopen(path, mode);
     if (fil == NULL)
     {
-        printf("Cannot open file '%s' with mode '%s'\n", path, mode);
+        fprintf(stderr, "Cannot open file '%s' with mode '%s'\n", path, mode);
         exit(1);
     }
     return fil;
